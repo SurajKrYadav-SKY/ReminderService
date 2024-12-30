c1f47ea6dab9255ab71ef633661ac17cacdd5567
Suraj Kumar Yadav <suraj.mails99@gmail.com>
2024-12-30 13:42:24 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
 
-const { sendBasicEmail } = require("./services/email-service");
+// const { sendBasicEmail } = require("./services/email-service");
 
 const setupAndStartServer = () => {
   const app = express();
@@ -13,12 +13,12 @@ const setupAndStartServer = () => {
   app.listen(PORT, () => {
     console.log("Server started at port:", PORT);
 
-    sendBasicEmail(
-      "support@admin.com",
-      "backendstudies@gmail.com",
-      "This is a testing number 3 email",
-      "This is the third test with another email"
-    );
+    // sendBasicEmail(
+    //   "support@admin.com",
+    //   "backendstudies@gmail.com",
+    //   "This is a testing number 3 email",
+    //   "This is the third test with another email"
+    // );
   });
 };
 
